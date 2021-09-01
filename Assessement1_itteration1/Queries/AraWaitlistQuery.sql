fa783f5cf81e7fecb79d0f1f8a9c9230f39624bf
keagan erasmus <keaganerasmus12@gmail.com>
2021-09-01 15:08:03 +1200

    added final version of the ERD, and fixed my SQL server to mimic my ERD

--- Assessement1_itteration1/Queries/AraWaitlistQuery.sql
+++ Assessement1_itteration1/Queries/AraWaitlistQuery.sql
@@ -5,37 +5,33 @@ GO
 DROP TABLE [dbo].[Patient]
 DROP TABLE [dbo].[Surgeon]
 DROP TABLE [dbo].[Refferer]
-DROP TABLE [dbo].[FSA]
 DROP TABLE [dbo].[Refferal]
 --DROP TABLE 
-
 
 
 ---CREATING PATIENT TABLE
 CREATE TABLE Patient(
 patientID INT IDENTITY(1,1) PRIMARY KEY,
-NHI VARCHAR(7) NOT NULL,
-firstName VARCHAR(25) NOT NULL,
-lastName VARCHAR(25) NOT NULL,
-DOB DATE NOT NULL,
-gender CHAR(15) NOT NULL
+NHI VARCHAR(7),
+firstName VARCHAR(25),
+lastName VARCHAR(25),
+DOB DATE,
+gender VARCHAR(15)
 )
 
 
 ---CREATING SURGEON TABLE
 CREATE TABLE Surgeon(
 surgeonId INT IDENTITY(1,1) PRIMARY KEY,
-fsaID INT FOREIGN KEY (fsaID) REFERENCES [dbo].[FSA](fsaID),
-firstName VARCHAR(25) NOT NULL,
-lastName VARCHAR(25) NOT NULL,
-department VARCHAR(25) NOT NULL
+firstName VARCHAR(25),
+lastName VARCHAR(25),
+department VARCHAR(25)
 )
 
 
 ---CREATING REFFERER TABLE
 CREATE TABLE Refferer(
 reffererID INT IDENTITY(1,1) PRIMARY KEY,
-surgeonId INT FOREIGN KEY (surgeonId) REFERENCES [dbo].[Surgeon](surgeonId),
 firstName VARCHAR(25),
 lastName VARCHAR(25) ,
 )
@@ -44,17 +40,17 @@ lastName VARCHAR(25) ,
 CREATE TABLE Refferal(
 refferalCode INT IDENTITY(1,1) PRIMARY KEY,
 
-patientID INT,
-FOREIGN KEY (patientID) REFERENCES [dbo].[Patient](patientID),
-
 surgeonID INT,
 FOREIGN KEY (surgeonID) REFERENCES [dbo].[Surgeon](surgeonID),
 
-refferalDate Date NOT NULL,
-refferedFrom VARCHAR(25) NOT NULL
-)
+patientID INT,
+FOREIGN KEY (patientID) REFERENCES [dbo].[Patient](patientID),
 
-CREATE TABLE FSA(
-fsaID INT IDENTITY(1,1) PRIMARY KEY,
-fsaDate DATE,
+
+reffererID INT,
+FOREIGN KEY (reffererID) REFERENCES [dbo].[Refferer](reffererID),
+
+refferalDate Date,
+refferedFrom VARCHAR(25),
+fsaDate DATE
 )
